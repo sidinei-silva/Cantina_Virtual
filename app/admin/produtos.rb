e912d3f71e63eeecf90c51eca8c11962d3d4f8dd
Sidinei Silva <sidinei.silva02@gmail.com>
2018-08-13 01:51:23 -0300

    Add Produto in active admin

---
+++ app/admin/produtos.rb
@@ -0,0 +1,41 @@
+ActiveAdmin.register Produto do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+  menu priority: 2
+  permit_params :nome_produto, :descricao_produto, :preco_produto, :image
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  action_item only: :show do
+    link_to 'Voltar', admin_produtos_path
+  end
+
+  form do |form|
+    form.semantic_errors *form.object.errors.keys
+    form.inputs "Produto" do
+      form.input :nome_produto
+      form.input :preco_produto, step: 0.1
+      form.input :image, as: :file
+      form.input :descricao_produto
+    end
+    form.actions
+  end
+
+  show :title => :nome_produto do
+    attributes_table do
+      default_main_content
+      row :image do |ad|
+        image_tag url_for(ad.image)
+      end
+    end
+  end
+  config.comments = false
+
+end
