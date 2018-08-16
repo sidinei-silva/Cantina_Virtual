2a960bb56838c7e147edcc5a27a4c0f40f43f58a
Sidinei Silva <sidinei.silva02@gmail.com>
2018-08-16 17:26:51 -0300

    Dashboard admin, falta ações

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,16 +4,21 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc {I18n.t("active_admin.dashboard")} do
 
-
     panel 'Recent Orders' do
-      table_for Pedido.all.order('id desc').limit(10) do
+
+      table_for Pedido.all.order('id desc').limit(1) do
         column 'Id do pedido', :id
         column 'Cliente', :cliente, :nome_cliente
         column 'Data do pedido', :created_at
         column('Status') {|pedido| status_tag(pedido.status_pedido)}
         column('Total pedido') {|pedido| number_to_currency pedido.total_pedido}
+
       end
     end
+    panel 'Recente 2' do
+      render 'admin/pedidos/recents'
+    end
+
 
 
     # Here is an example of a simple dashboard with columns and panels.
